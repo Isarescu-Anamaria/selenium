478c491181a6a2da514ef422310e2bbfacdbea45
Isarescu <anamaria.isarescu.ext@siemens.com>
2024-08-01 12:44:39 +0300

    test

---
+++ test/src/main/java/TestHomePage.java
@@ -0,0 +1,41 @@
+import java.util.concurrent.TimeUnit;
+//--
+import org.openqa.selenium.By;
+//--
+import org.openqa.selenium.WebDriver;
+//--
+import org.openqa.selenium.WebElement;
+//--
+import org.openqa.selenium.chrome.ChromeDriver;
+
+//--
+import org.testng.Assert;
+//--
+import org.testng.annotations.AfterClass;
+//--
+import org.testng.annotations.BeforeClass;
+//--
+import org.testng.annotations.Test;
+
+public class TestHomePage {
+
+    private WebDriver driver;
+
+    @BeforeClass
+    public void beforeClass() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+
+    @Test
+    public void testBookNowButton() {
+
+        driver.get("https://ancabota09.wixsite.com/intern");
+
+
+    }
+}
